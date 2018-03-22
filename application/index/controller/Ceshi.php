3b6175ae9f70ae9c4a4b7d53099f49e52f1df2e6
liuxiaojie <1206973948@qq.com>
2018-03-22 15:11:19 +0800

    第一次上传

---
+++ application/index/controller/Ceshi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\index\controller;
+
+class Ceshi{
+
+    public function ceshi(){
+        echo 111;exit;
+    }
+
+}
---
+++ application/index/controller/admin/Index.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\index\controller\admin;
+
+class Index
+{
+    public function index(){
+        echo 1;exit;
+    }
+}
